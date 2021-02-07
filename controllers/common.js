fa405795c5f872702122ffaa39ffff0fc8325194
Klaur <zhangleping613@163.com>
2021-02-07 14:59:18 +0800

    优化图片上传

--- controllers/common.js
+++ controllers/common.js
@@ -5,12 +5,15 @@ const fileUtils = require('../utils/file')
 module.exports = {
   async upload(ctx, next) {
     const img = await new Image({
-      url: `${ctx.origin}/${ctx.uploadpath.file}`,
+      url: `/${ctx.uploadpath.file}`,
       md5: ctx.md5
     }).save()
     if (img) {
       ctx.body = message({
-        img
+        img: {
+          id: img._id,
+          url: `${ctx.origin}/${ctx.uploadpath.file}`
+        }
       })
     } else {
       ctx.body = message(img)
--- middlewares/image.js
+++ middlewares/image.js
@@ -2,21 +2,26 @@ const md5 = require('md5')
 const fs = require('fs')
 const message = require('../utils/message')
 const Image = require('../models/image')
+const image = require('../models/image')
 const checkExist = async (ctx, next) => {
   const file = ctx.request.files.file
   let buf = fs.readFileSync(file.path)
   if (buf) {
     let md5Code = md5(buf)
-    console.log('-------------', md5Code)
+    // console.log('-------------', md5Code)
     ctx.md5 = md5Code
     const img = await Image.findOne({ md5: md5Code })
     if (img) {
       fs.rmdirSync(file.path, { recursive: true })
       ctx.body = message({
-        img
+        img: {
+          url: `${ctx.origin}${img.url}`,
+          id: img._id
+        }
       })
     } else {
-      next()
+      console.log('未找到')
+      await next()
     }
   } else {
     ctx.body = message(err)
